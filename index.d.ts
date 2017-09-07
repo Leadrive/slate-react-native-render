ac622e7cde9e93c6d6a2462839fd7d7baa51afc9
Chung <birdsing9@gmail.com>
2017-09-07 16:35:14 +0800

    Update

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,13 @@
-export {Raw} from './lib/Raw'
-export {RNHtml} from './lib/RNHtml'
+//export {Raw} from './lib/Raw'
+//export {RNHtml} from './lib/RNHtml'
+
+declare module "slate-react-native-render" {
+    export class RNHtml {
+        serialize(state:any)
+        constructor(rules:any[])
+    }
+
+    export const Raw:{
+        deserialize:(object:any, options:{})=>{}
+    }
+}
